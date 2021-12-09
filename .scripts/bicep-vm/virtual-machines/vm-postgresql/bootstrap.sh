294ac8d3066e0d83a853e80bca93f384d080ae45
Samuel Rowe <samrowe@deneb.co.uk>
2021-12-09 16:11:35 +0000

    Fixes pg install

--- .scripts/bicep-vm/virtual-machines/vm-postgresql/bootstrap.sh
+++ .scripts/bicep-vm/virtual-machines/vm-postgresql/bootstrap.sh
@@ -22,8 +22,17 @@ EOF
 
 systemctl status wildfly
 
-# NEED TO SETUP WILDFLY ADMIN user HERE
+##############
+# HARDCODED ADMIN PASSWORD! DO NOT USE IN ANYTHING RESEMBLING PRODUCTION!!!!
+# FOR DEMO/WORKSHOP PURPOSES ONLY!!!! 
+##############
+/opt/wildfly/bin/add-user.sh -u 'adminuser1' -p 'password1!' -g 'admin'
+##############
+# YOU HAVE BEEN WARNED
+##############
+
 # Change Wildfly standalone.sh to accept input from 0.0.0.0
+sed -i 's#$WILDFLY_HOME/bin/standalone.sh -c $2 -b $3#$WILDFLY_HOME/bin/standalone.sh -c $2 -b $3 -bmanagement=0.0.0.0#g' /opt/wildfly/bin/launch.sh
 
 systemctl restart wildfly
 
@@ -35,8 +44,26 @@ sudo postgresql-setup --initdb
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
-# NEED TO CHANGE pg_hba.conf to listen on all addresses for IPv4
+##############
+# HARDCODED POSTGRESS ALLOW ALL HOST BASED AUTH! DO NOT USE IN ANYTHING RESEMBLING PRODUCTION!!!!
+# FOR DEMO/WORKSHOP PURPOSES ONLY!!!! 
+##############
+sed -i 's#local   all             all                                     ident#local   all             all                                     trust#g' /var/lib/pgsql/data/pg_hba.conf
+sed -i 's#host    all             all             127.0.0.1/32            ident#host    all             all             0.0.0.0/0               password#g' /var/lib/pgsql/data/pg_hba.conf
+##############
+# YOU HAVE BEEN WARNED (Again)
+##############
+
+
+# Need to change the postgresql.conf to listen on all addresses too
+sed -i 's|#listen_addresses|listen_addresses|g' /var/lib/pgsql/data/postgresql.conf
+
 
 sudo systemctl restart postgresql
 
 # Need to setup password for PGUser
+cp /root/SetPostgresUserAccountPassword.sql /var/lib/pgsql
+cd /var/lib/pgsql
+chmod u+r SetPostgresUserAccountPassword.sql
+chown postgres:postgres SetPostgresUserAccountPassword.sql
+sudo -u postgres psql -U postgres postgres -f /var/lib/pgsql/SetPostgresUserAccountPassword.sql
